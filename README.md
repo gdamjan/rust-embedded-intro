f108ea21f8cef6674291646a8764d111ab136387
Дамјан Георгиевски <gdamjan@gmail.com>
2024-05-17 19:51:45 +0200

    cargo add cortex-m-rt, panic-halt
    `src/main.rs` is now an empty template for an embedded main loop
    
    .. code will not compile at this point

---
+++ README.md
@@ -0,0 +1,8 @@
+# `Rust Embedded Intro`
+
+Using the [`nrf8340-dk`](https://www.nordicsemi.com/Products/Development-hardware/nrf5340-dk)
+board based on Cortex-M33 with the `thumbv8m.main-none-eabihf` ISA, with 1MB Flash and 256K RAM.
+
+## Preparation
+
+- [`rustup`](https://rustup.rs/) - is recommended to install rust and its components
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+#![no_main]
+#![no_std]
+
+// Some panic handler needs to be included. This one halts the processor on panic.
+use panic_halt as _;
+
+// Use `main` as the entry point of this application, which may never return.
+#[cortex_m_rt::entry]
+fn main() -> ! {
+    // initialization
+
+    loop {
+        // application logic
+    }
 }
