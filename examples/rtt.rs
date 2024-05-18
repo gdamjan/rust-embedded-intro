abab5b4a82ab606bb9cadc7b1f60834d754c5f83
Дамјан Георгиевски <gdamjan@gmail.com>
2024-05-18 04:12:35 +0200

    fix rtt demo
    since the embassy demo added embassy-nrf, and with that pulled both
    cortex-m-rt and nrf5340-app-pac, now we explicitly need to set the
    vectore table.
    
    previously, cortex-m-rt was setting a generic vector table

--- examples/rtt.rs
+++ examples/rtt.rs
@@ -7,6 +7,11 @@ use panic_halt as _;
 // include cortex_m for the critical_section implementation required by rtt_target
 use cortex_m as _;
 
+// cortex_m_rt no longer sets up the vector table, since embassy-nrf pulled it with
+// the device feature  so we need to do that ourselves.
+// https://docs.rs/cortex-m-rt/latest/cortex_m_rt/
+use nrf5340_app_pac as _;
+
 use rtt_target::{rprintln, rtt_init_print};
 
 // Use `main` as the entry point of this application, which may never return.
